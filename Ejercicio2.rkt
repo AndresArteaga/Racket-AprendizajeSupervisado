986a6a4d97be3a0ba66f187f6756f9353d059096
AndresArteaga <andress-zz-15@hotmail.com>
2017-01-30 18:00:47 -0500

    Add files via upload

---
+++ Ejercicio2.rkt
@@ -0,0 +1,9 @@
+#|Andrés Sebastián Arteaga Cabezas|#
+;Ejercicio 2
+
+(define (siguiente l)
+  (map (lambda (x) (+ x 1)) l))
+
+;;Ejecución
+;;=====================================
+;(siguiente '(1 2 3 4))
